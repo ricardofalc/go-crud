31815c388275fb069b8e911d22c2cf668811ee40
ricardofalc <90937422+ricardofalc@users.noreply.github.com>
2023-10-09 13:24:13 +0200

    dbstring

--- initializers/database.go
+++ initializers/database.go
@@ -12,8 +12,6 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 
-	//postgres://ntomzeat:TguTkpt4FUIfBvLM2yzT4g5xHztIfZ8s@ella.db.elephantsql.com/
-
 	var err error
 	dsn := os.Getenv("DB_STRING")
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
